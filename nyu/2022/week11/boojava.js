a57717d11655de6685f3ead799b7221428625871
jin-kairui <53275459+jin-kairui@users.noreply.github.com>
2022-04-19 04:27:58 -0400

    Update boojava.js

--- nyu/2022/week11/boojava.js
+++ nyu/2022/week11/boojava.js
@@ -1,38 +1,19 @@
 // ghost img 
 let ghost = document.getElementById('ghost');
+let ghost_run = document.getElementById('ghost_run');
 let ghost_img = document.getElementById("ghost_img");
 
 //text
-let text = document.getElementById('text');
 let textbox = document.getElementById('textbox');
 
-//background scenery 
-let scenery = document.getElementById("time");
-
 //status img 
-let status_bar = document.getElementById("status_bar");
+let status_img = document.getElementById("status_img");
 var statusCounter = 0;
+//0-4 sad, 5-9 yellow, 10-14 green, 15-19 green green
 
-if (statusCounter == 1){
-    $(document).ready(function() {
-        $(status_bar).attr("src", "assets/yellowbar.PNG");
-    });
-}
-
-function upStatus() {
-    statusCounter += 1;
-    document.getElementById("happiness").innerHTML = happiness;
-};
-
-function resetStatus() {
-    statusCounter = 0;
-};
-
-$(document).ready(function() {
-    $(text).click(function() {
-        $(status_bar).attr("src", "assets/yellowbar.PNG");
-    });
-});
+//game background 
+let game_container = document.getElementById("game_container");
+var isDaytime = false;
 
 //action buttons 
 let love = document.getElementById("love");
@@ -43,25 +24,105 @@ let clock = document.getElementById("clock");
 //body 
 let theBody = document.querySelector('body');
 
+//audio media
+const musicAudio = new Audio("assets/ES_Sunshine Song - The Waiting World.mp3");
+musicAudio.volume = 0.2;
+const statusAudio = new Audio("assets/statusbarUP.mp3");
+const booTalksAudio = new Audio("assets/bootalks.mp3");
+const heartAudio = new Audio("assets/heart.mp3");
+
+//ghost name
+//var ghost_name = 'MeiNa';
+
+/////////////////////////////////////////////////////////////////
+
+function upStatus() {
+    statusCounter = statusCounter + 1;
+    let ghost_name = sessionStorage.getItem("ghost_name");
+    if (statusCounter == 0 || statusCounter == 5 || statusCounter == 10 || statusCounter == 15) {
+        statusAudio.play();
+    }
+    if (0 <= statusCounter && statusCounter <= 4) {
+        $(status_img).attr("src", "assets/redbar.PNG");
+        $(textbox).html(ghost_name + "'s &nbsp;&nbsp;feeling &nbsp;&nbsp;lonely.");
+
+    } else if (5 <= statusCounter && statusCounter <= 9) {
+        $(status_img).attr("src", "assets/yellowbar.PNG");
+        $(textbox).html(ghost_name + "&nbsp;&nbsp;wants &nbsp;&nbsp;to &nbsp;&nbsp;play &nbsp;&nbsp;more!");
+        //setInterval(resetStatus, 5000);
+
+    } else if (10 <= statusCounter && statusCounter <= 14) {     
+        $(status_img).attr("src", "assets/greenbar.PNG");
+        $(textbox).html(ghost_name + "&nbsp;&nbsp;is &nbsp;&nbsp;feeling &nbsp;&nbsp;happy!");
+        //setInterval(resetStatus, 5000);
+
+    } else {       
+        $(status_img).attr("src", "assets/2greenbar.PNG");
+        $(textbox).html(ghost_name + " &nbsp;&nbsp;loves &nbsp;&nbsp;your &nbsp;&nbsp;company!");
+        //setInterval(resetStatus, 5000);
+    } 
+};
+
+function resetStatus() {
+    statusCounter = 0;
+    let ghost_name = sessionStorage.getItem("ghost_name");
+    if (statusCounter == 0) {
+        $(textbox).html("You&nbsp;&nbsp; can't&nbsp;&nbsp;touch &nbsp;&nbsp;" + ghost_name + "!");
+        $(status_img).attr("src", "assets/redbar.PNG");
+    }
+    //stop da music
+    if(musicAudio.played) {
+        musicAudio.pause();
+    }
+};
+
+//timer to reset status bar 
+statusInterval = setInterval(statusDown, 5000);
+function statusDown() {
+    statusCounter - 5;
+}
+
+//button functions 
 $(document).ready(function() {
     $(love).click(function() {
         $(ghost_img).attr("src", "assets/heartboo.PNG");
+        heartAudio.play();
+        clearInterval(statusInterval);
+        upStatus();
+        //setInterval(statusDown, 5000);
     });
-    upStatus();
+    
 });
 
 $(document).ready(function() {
     $(chat).click(function() {
         $(ghost_img).attr("src", "assets/chatboo.PNG");
+        booTalksAudio.play();
+        clearInterval(statusInterval);
+        upStatus();
+        //setInterval(statusDown, 5000);
     });
+    
 });
 
 $(document).ready(function() {
     $(music).click(function() {
         $(ghost_img).attr("src", "assets/danceboo.PNG");
-        $(status_bar).attr("src", "assets/yellowbar.PNG");
-        // play music here 
+
+        // play or pause music by clicking on music
+        if (musicAudio.paused) {
+            musicAudio.play();
+        } else {
+            musicAudio.pause();
+        }
+
+        //move back and forth
+
+        clearInterval(statusInterval);
+        upStatus();
+        //setInterval(statusDown, 5000);
     });
+    
 });
 
 // reset ghost to normal 
@@ -73,9 +134,34 @@ $(document).ready(function() {
     });
 });
 
-// get the clock to choose the other background no matter what it's on 
+// click clock to switch day/night background
 $(document).ready(function() {
     $(clock).click(function() {
-        $(scenery).attr("src", "assets/day.PNG");
+        if (isDaytime) {
+            $(game_container).css({"background-image": "url('assets/night.PNG')"});
+            isDaytime = false;
+        } else {
+            $(game_container).css({"background-image": "url('assets/day.PNG')"});
+            isDaytime = true;
+        }
     });
 });
+
+
+//user name input 
+let submit_name = document.getElementById("name_form_submit");
+
+$(document).ready(function() {
+    $("#nameForm").submit(function() {
+
+        var ghost_name = $("#ghost_name").val();
+        alert(ghost_name);
+        sessionStorage.setItem("ghost_name", ghost_name);
+    })
+})
+
+$(document).ready(function() {
+    $(submit_name).click(function(){
+        window.location.href = "booindex.html";
+    })
+})
